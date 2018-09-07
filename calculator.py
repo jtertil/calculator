bdae8c3f22da7e4e76b06d6f368de5bcdc234ba8
jtertil <jtertil@gmail.com>
2018-09-07 10:18:32 +0200

    OOP version

--- calculator.py
+++ calculator.py
@@ -1,20 +1,50 @@
-(value1) = float(input("value1? : "))
-(value2) = float(input("vailue2? : "))
+x = y = z = 0
 
-print("type of calculation (1-add, 2-substract, 3-multiply, 4-divide: ")
-select = float(input())
+com1 = "Please use only numbers and dot (.) as decimal separator"
+com2 = "Please select: 1-add, 2-substract, 3-multiply, 4-divide: "
+com_e = "Sorry, I can't do this. "
 
-if select == 1:
-    result = value1 + value2
-    print("add result: ", result)
-elif select == 2:
-    result = value1 - value2
-    print("substract result: ", result)
-elif select == 3:
-    result = value1 * value2
-    print("multiply result: ", result)
-elif select == 4:
-    result = value1 / value2
-    print("divide result: ", result)
+while type(x) != float:
+    try:
+        x = float(input("x: "))
+    except ValueError:
+        print (com_e + com1)
+
+while type(y) != float:
+    try:
+        y = float(input("y: "))
+    except ValueError:
+        print (com_e + com1)
+
+while type(z) != float or ((z<1) or (z>4)):
+    try:
+        z = float(input(com2))
+    except ValueError:
+        print (com_e)
+
+class calculator:
+    def addition(x,y):
+        print("Add result for %d and %d is: " % (x, y), x+y)
+
+    def subtraction(x,y):
+        print("Substract result for %d and %d is: " % (x, y), x-y)
+
+    def multiplication(x,y):
+        print("Multiply result for %d and %d is: " % (x, y), x*y)
+
+    def division(x,y):
+        try:
+            print("Divide result for %d and %d is: " % (x, y), x/y)
+        except ZeroDivisionError as err:
+            print(com_e + "(" + str(err) + ")")
+
+if z == 1:
+    calculator.addition(x,y)
+elif z == 2:
+    calculator.subtraction(x,y)
+elif z == 3:
+    calculator.multiplication(x,y)
+elif z == 4:
+    calculator.division(x,y)
 else:
-    print("error")
+    print("Something goes wrong")
